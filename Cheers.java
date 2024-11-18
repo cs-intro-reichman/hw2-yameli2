aca695ee0bdd71c59884151527633ce761c57609
yameli2 <yam.elimelech@post.runi.ac.il>
2024-11-18 22:13:26 +0200

    Done

--- Cheers.java
+++ Cheers.java
@@ -3,11 +3,75 @@ public class Cheers {
         public static void main(String[] args) {
                 String cheerWord = args[0];
                 int numOfCheer = Integer.parseInt(args[1]);
-                cheerWord = cheerWord.toUpperCase();
-                String letters = "AEFHILMNORSX";
+                String ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                //String abc = "abcdefghijklmnopqrstuvwxyz";
+                String capitalCheerWord = " ";
+                //runi
+                for(int j=0;j<ABC.length();j++)
+                {
+                 char letter = cheerWord.charAt(j);
+                 if(ABC.indexOf(letter) == -1){
+                switch (letter) {
+                    case 'a':  capitalCheerWord += "A";
+                             break;
+                    case 'b':  capitalCheerWord += "B";
+                             break;
+                    case 'c':  capitalCheerWord += "C";
+                             break;
+                    case 'd':  capitalCheerWord += "D";
+                             break;
+                    case 'e':  capitalCheerWord += "E";
+                             break;
+                    case 'f':  capitalCheerWord += "F";
+                             break;
+                    case 'g':  capitalCheerWord += "G";
+                             break;
+                    case 'h':  capitalCheerWord += "H";
+                             break;
+                    case 'i':  capitalCheerWord += "I";;
+                             break;
+                    case 'j':  capitalCheerWord += "J";
+                             break;
+                    case 'k':  capitalCheerWord += "K";
+                             break;
+                    case 'l':  capitalCheerWord += "L";
+                             break;
+                    case 'm':  capitalCheerWord += "M";
+                             break;
+                    case 'n':  capitalCheerWord += "N";
+                             break;
+                    case 'o':  capitalCheerWord += "O";
+                             break;
+                    case 'p':  capitalCheerWord += "P";
+                             break;
+                    case 'q':  capitalCheerWord += "Q";
+                             break;
+                    case 'r':  capitalCheerWord += "R";
+                             break;
+                    case 's':  capitalCheerWord += "S";
+                             break;
+                    case 't':  capitalCheerWord += "T";
+                             break;
+                    case 'u':  capitalCheerWord += "U";
+                             break;
+                    case 'v':  capitalCheerWord += "V";
+                             break;
+                    case 'w':  capitalCheerWord += "W";
+                             break;
+                    case 'x':  capitalCheerWord += "X";
+                             break;
+                    case 'y':  capitalCheerWord += "Y";
+                             break;
+                    case 'z':  capitalCheerWord += "Z";
+                             break;
+                }
+        }
+                }
+               // cheerWord = cheerWord.toUpperCase();
+                String addAnLetters = "AEFHILMNORSX";
                 for(int i =0; i<cheerWord.length(); i++)
                 {
-                        if (letters.indexOf(cheerWord) == -1){
+                        if (addAnLetters.indexOf(cheerWord) == -1){
                                 System.out.println("GIVE ME a "+cheerWord.charAt(i)+": "+cheerWord.charAt(i)+"!");      
                         }
                         else
