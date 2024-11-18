ba2d438b5631f5d68ebd605a12ab1d3917d220bc
yameli2 <yam.elimelech@post.runi.ac.il>
2024-11-18 20:06:30 +0200

    done

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,22 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String cheerWord = args[0];
+                int numOfCheer = Integer.parseInt(args[1]);
+                cheerWord = cheerWord.toUpperCase();
+                String letters = "AEFHILMNORSX";
+                for(int i =0; i<cheerWord.length(); i++)
+                {
+                        if (letters.indexOf(cheerWord) == -1){
+                                System.out.println("GIVE ME a "+cheerWord.charAt(i)+": "+cheerWord.charAt(i)+"!");      
+                        }
+                        else
+                        {
+                                System.out.println("GIVE ME an "+cheerWord.charAt(i)+": "+cheerWord.charAt(i)+"!");
+                        }
+                }
+                System.out.println("What does it spell?");
+                for(int b=0;b<cheerWord.length();b++)
+                        System.out.println(cheerWord+"!!!");   
         }
 }
